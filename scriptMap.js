6bb51d749bfcf01394990d19053d92aa952dd2d8
Quang Thien Le <thienlequang62ld@gmail.com>
2024-04-09 19:15:26 +0700

    add note to map

--- scriptMap.js
+++ scriptMap.js
@@ -304,6 +304,15 @@ const drawMap = (year, title) => {
         .style("border-radius", "5px")
         .style("background", "")
         .call(zoom); // Apply the zoom behavior to the SVG
+
+    svg.append('text')
+        .attr('class', 'note')
+        .text('Click the left mouse button to select countries and view their data.')
+        .style('font-size', '12px')
+        .attr('x', 10)
+        .attr('y', h - 20);
+
+
     year = Number(year);
     var yearStr = String(year);
     // Get the last two digits of the next year
